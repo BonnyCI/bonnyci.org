314d664dd23b1b7ce4a42d0217f893fa755c2191
Matt Langbehn <mlangbe@us.ibm.com>
2017-01-17 11:27:26 -0600

    Added Travis CI integration
    Added:
    - Basic Travis CI configuration
    - Basic Textlint configuration
      - The following rules were added:
        - [No Todo](https://github.com/azu/textlint-rule-no-todo)
          - Fails if TODO sections exist
        - [No Empty Section](https://github.com/azu/textlint-rule-no-empty-section)
          - Fails if there is an empty section
        - [Common Misspellings](https://github.com/io-monad/textlint-rule-common-misspellings)
          - Checks for commonly misspelled words
        - [Alex](https://github.com/azu/textlint-rule-alex)
          - Checks for potentially offensive language
        - [Rousseau](https://github.com/azu/textlint-rule-rousseau)
          - Lightweight proofreader
    
    Edits:
    - Disabled email notifications from Travis CI
      - Thanks @eggshell for pointing this out
    
    Signed-off-by: Matt Langbehn <mlangbe@us.ibm.com>

---
+++ .textlintrc.js
@@ -0,0 +1,18 @@
+"use strict"
+module.exports = {
+  "rules": {
+    "no-todo": {
+    },
+    "no-empty-section": {
+    },
+    "common-misspellings": {
+      "ignore": []
+    },
+    "alex": {
+    },
+    "rousseau": {
+      "ignoreTypes": [],
+      "showLevels": ["suggestion", "warning", "error"]
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # BonnyCI Documentaion
+Documentation for [BonnyCI](https://github.com/BonnyCI).
