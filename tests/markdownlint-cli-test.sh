4003a7bb9c8d9836a971b624d837632b2cf04702
Matt Langbehn <matthew.langbehn@gmail.com>
2017-02-20 14:29:40 -0600

    Add message about success or failure to CI tests
    Previously, some of the tests in ./tests/ did not print a success
    message, making CI logs difficult to understand. Now all tests print a
    message on success or failure.
    
    Related-Issue: BonnyCI/bonnyci.org#24
    Signed-off-by: Matt Langbehn <matthew.langbehn@gmail.com>

--- tests/markdownlint-cli-test.sh
+++ tests/markdownlint-cli-test.sh
@@ -2,4 +2,10 @@
 
 sudo npm install -g markdownlint-cli
 
-markdownlint -c .markdownlint.js .
+if ! markdownlint -c .markdownlint.js .; then
+    echo "ERROR: Markdown linting failed!"
+    echo "SUGGESTION: See messages above for specific issues."
+    exit 1
+else
+    echo "SUCCESS: Markdown linting passed!"
+fi
